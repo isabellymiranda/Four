50f6dd1c0b5ad74ec00c345042a1701981e6c28f
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2022-03-11 02:04:08 +0000

    envio

---
+++ Four.java
@@ -0,0 +1,12 @@
+public class Four {
+    public static void main(String[] args) { 
+    int fatorial = 1;
+System.out.println("A fatoração é:");
+for (int i = 1; i <= 100; i++){
+    fatorial = i * fatorial;
+    System.out.println(fatorial);  
+}
+} 
+}
+
+
